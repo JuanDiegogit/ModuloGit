302cb39617ba48f5e45e7cff2b486819ae883022
Juan Diego Mejia Maestre <jdmm31152814@gmail.com>
2020-05-11 20:51:10 -0500

    Creacion completa de la capa para realizar consultas al API

---
+++ Controlador/Respuesta.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controlador
+{
+    /// <summary>
+    /// Este clases sirve de plantilla para devolver la respuesta del servidor
+    /// </summary>
+    class Respuesta<T>
+    {
+        public string Mensaje {set;get;}
+        public T Objeto { set;get; }
+
+      
+
+        public async static Task<Respuesta<T>> GetRespuesta<T> (HttpResponseMessage httpResponseMessage) 
+        {
+            string mensaje = httpResponseMessage.StatusCode.ToString();
+            T objeto = default(T);
+            try
+            {
+
+             objeto   = Newtonsoft.Json.JsonConvert.DeserializeObject<T>(await httpResponseMessage.Content.ReadAsStringAsync());
+
+            }
+            catch (Exception)
+            {
+
+            }
+            return new Respuesta<T>() { Mensaje = mensaje,Objeto = objeto };
+        }
+    }
+}
